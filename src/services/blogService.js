d45de8472a5b637addbfabcaf5379494527b618d
Ben Manley <benjamintmanley@gmail.com>
2023-10-11 14:44:48 -0500

    implement API call for blog show

--- src/services/blogService.js
+++ src/services/blogService.js
@@ -15,7 +15,21 @@ async function index () {
   }
 }
 
+async function show(blogId) {
+  try {
+    const res = await fetch(`${BASE_URL}/${blogId}`, {
+      headers: {
+        'Authorization': `Bearer ${tokenService.getToken()}`
+      }
+    })
+    return res.json()
+  } catch (error) {
+    console.log(error)
+  }
+}
+
 
 export {
-  index
+  index,
+  show
 }
